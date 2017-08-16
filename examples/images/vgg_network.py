a899cee8063e27f76036c46b7ba8a0155230c3d9
hash <hash>
2017-08-16 19:13:53 +0800

    vgg_network

--- examples/images/vgg_network.py
+++ examples/images/vgg_network.py
@@ -21,9 +21,11 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 
+datapath = '../../data/17flowers224'
+
 # Data loading and preprocessing
 import tflearn.datasets.oxflower17 as oxflower17
-X, Y = oxflower17.load_data(one_hot=True)
+X, Y = oxflower17.load_data(one_hot=True, dirname=datapath)
 
 # Building 'VGG Network'
 network = input_data(shape=[None, 224, 224, 3])
@@ -62,8 +64,8 @@ network = regression(network, optimizer='rmsprop',
                      learning_rate=0.0001)
 
 # Training
-model = tflearn.DNN(network, checkpoint_path='model_vgg',
-                    max_checkpoints=1, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=500, shuffle=True,
-          show_metric=True, batch_size=32, snapshot_step=500,
+model = tflearn.DNN(network, checkpoint_path='vn-ckpt/model_vgg',
+                    max_checkpoints=3, tensorboard_verbose=0)
+model.fit(X, Y, n_epoch=3, shuffle=True,
+          show_metric=True, batch_size=16, snapshot_step=50,
           snapshot_epoch=False, run_id='vgg_oxflowers17')
