a8d5fe15522cd854e7f05f8c4c323daa2c621f1d
hash <hash>
2017-08-16 18:17:28 +0800

    convnet_mnist

--- examples/images/convnet_mnist.py
+++ examples/images/convnet_mnist.py
@@ -20,9 +20,11 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
 
+datapath = '../../data/mnist'
+
 # Data loading and preprocessing
 import tflearn.datasets.mnist as mnist
-X, Y, testX, testY = mnist.load_data(one_hot=True)
+X, Y, testX, testY = mnist.load_data(one_hot=True, data_dir=datapath)
 X = X.reshape([-1, 28, 28, 1])
 testX = testX.reshape([-1, 28, 28, 1])
 
@@ -43,7 +45,8 @@ network = regression(network, optimizer='adam', learning_rate=0.01,
                      loss='categorical_crossentropy', name='target')
 
 # Training
-model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit({'input': X}, {'target': Y}, n_epoch=20,
+model = tflearn.DNN(network, tensorboard_verbose=0,
+                    checkpoint_path='cm-ckpt/model.cm.ckpt', max_checkpoints=3)
+model.fit({'input': X}, {'target': Y}, n_epoch=10,
            validation_set=({'input': testX}, {'target': testY}),
            snapshot_step=100, show_metric=True, run_id='convnet_mnist')
